cf9ecaa7f94c28996ef3a9964c45383a6de6dbbf
volkan <volkanilkyar@gmail.com>
2022-04-01 10:46:44 +0300

    consol girişi konusu eklendi

--- lib/console_input.dart
+++ lib/console_input.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main(List<String> args) {
   print("isminizi girin");
   String name = stdin
-      .readLineSync()!; //klayveyeden ismi alıyoruz ve name değişkenine aktarıyoruz
+      .readLineSync()!; //klayveyeden ismi alıyoruz ve name değişkenine aktarıyoruz.
 
   print("isminiz $name");
 }
